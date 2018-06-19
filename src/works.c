171b1c0b39f3f209ba5ad639d1c40266cc90dbf5
T32aks <T32aks@gmail.com>
2018-06-19 16:32:20 +0900

    solved 3.4

--- src/works.c
+++ src/works.c
@@ -4,6 +4,9 @@ int n;
 int k;
 int A[100000];
 
+int ge(int m);
+int sum();
+int max();
 
 int main(){
   int i, lb, ub;
@@ -11,7 +14,44 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = max() - 1;
+  ub = sum();
+  while (ub - lb > 1) {
+    int m = (lb + ub) / 2;
+    if (ge(m) <= k) ub = m;
+    else lb = m;
+    printf("%d, %d\n", lb, ub);
+  }
+  printf("%d", ub);
   return 0;
+}
+
+int ge(int m) {
+  int i, res, tmp;
+  res = 0;
+  tmp = 0;
+  for (i = 0; i < n; i++) {
+    tmp += A[i];
+    if (tmp > m) {
+      res++;
+      tmp = A[i];
+    }
+  }
+  return ++res;
+}
+
+int sum() {
+  int i, res;
+  res = 0;
+  for(i = 0; i < n; i++) res += A[i];
+  return res;
+}
+
+int max() {
+  int i, res;
+  res = A[0];
+  for (i = 1; i < n; i++) {
+    if(res < A[i]) res = A[i];
+  }
+  return res;
 }
