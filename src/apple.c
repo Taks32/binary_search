8c53142eada1b5aa8e070392a12780088b18cb88
T32aks <T32aks@gmail.com>
2018-06-19 15:21:10 +0900

    🍎 できた

--- src/apple.c
+++ src/apple.c
@@ -4,14 +4,35 @@ int n;
 int k;
 int A[100000];
 
+int rec(int n, int m);
+int ltk(int num);
+
 
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
+  if (k < n) return -1;
+  for (i = 0; i < n; i++) {
     scanf("%d", &A[i]);
   }
-
-
+  lb = 1;
+  ub = A[n - 1];
+  if (ltk(1) < k) lb = ub = 1;
+  else while (ub - lb > 1) {
+    int m = (lb + ub) / 2;
+    if (ltk(m) <= k) ub = m;
+    else lb = m;
+  }
+  printf("%d", ub);
   return 0;
+}
+
+int ltk(int num) {
+  int i, res;
+  res = 0;
+  for (i = 0; i < n; i++) {
+    res += A[i] / num;
+    if (A[i] % num != 0) res++;
+  }
+  return res;
 }
