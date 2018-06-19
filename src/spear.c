4898d438c968fafc442e5be84aa838b79c26cc90
T32aks <T32aks@gmail.com>
2018-06-19 15:48:27 +0900

    solved 3.3

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,8 @@ int n;
 int k;
 int A[100000];
 
+int ge(int m);
+
 
 int main(){
   int i, lb, ub;
@@ -11,7 +13,23 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = A[n - 1];
+  while (ub - lb > 1) {
+    int m = (ub + lb) / 2;
+    if(ge(m) < k) ub = m;
+    else lb = m;
+    printf("%d, %d\n", ub, lb);
+  }
+  printf("%d", lb);
   return 0;
+}
+
+int ge(int m) {
+  int i, res;
+  res = 0;
+  for (i = 0; i < n; i++) {
+    res += A[i] / m;
+  }
+  return res;
 }
