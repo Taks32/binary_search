a2d3034b735b22c823d74fd64e831060a1411b13
T32aks <T32aks@gmail.com>
2018-06-19 15:01:18 +0900

    arrayの完成

--- src/array.c
+++ src/array.c
@@ -11,8 +11,21 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
-
+  if (A[0] > k) {
+    printf("%d", n);
+    return 0;
+  }
+  if (A[n - 1] < k) {
+    printf("%d", n);
+    return 0;
+  }
+  lb = 0;
+  ub = n;
+  while (ub - lb > 1) {
+    int m = (lb + ub) / 2;
+    if (A[m] > k) ub = m;
+    else lb = m;
+  }
+  printf("%d", ub);
   return 0;
 }
